1d58bdd2d5103c0e73fc6d4f312cdc596ee48583
Jason <kangja1996@gmail.com>
2021-10-02 01:18:48 -0400

    added cors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const proxyUrl = "https://secret-ocean-49799.herokuapp.com/";
+const stocksUrl = `${proxyUrl}https://query1.finance.yahoo.com/v8/finance/chart/GME`;
+async function getStocks() {
+  const response = await fetch(stocksUrl);
+  return response.json();
+}
 
 function App() {
+  useEffect(() => {
+    getStocks().then((data) => {
+      console.log(data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
