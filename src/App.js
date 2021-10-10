9e4453a8a553db288229c7c9f96df5a261dfc028
Jason <kangja1996@gmail.com>
2021-10-10 12:47:38 -0400

    changing classname

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 const proxyUrl = "https://secret-ocean-49799.herokuapp.com/";
 const stocksUrl = `${proxyUrl}https://query1.finance.yahoo.com/v8/finance/chart/GME`;
-
 async function getStocks() {
   const response = await fetch(stocksUrl);
 
@@ -11,19 +10,23 @@ async function getStocks() {
 
 function App() {
   const [price, setPrice] = useState(-1);
+  const [prevPrice, setPrevPrice] = useState(-1);
   const [priceTime, setPriceTime] = useState(null);
 
   useEffect(() => {
     let timeoutId;
     async function getLatestPrice() {
       const data = await getStocks();
+      console.log(data);
       const gme = data.chart.result[0];
+      setPrevPrice(price);
       setPrice("$" + gme.meta.regularMarketPrice.toFixed(2));
       setPriceTime(new Date(gme.meta.regularMarketTime * 1000));
-      timeoutId = setTimeout(getLatestPrice, 5000);
+      timeoutId = setTimeout(getLatestPrice, 5000 * 2);
     }
 
-    timeoutId = setTimeout(getLatestPrice, 5000);
+    // timeoutId = setTimeout(getLatestPrice, 5000);
+    getLatestPrice();
 
     return () => {
       clearTimeout(timeoutId);
@@ -32,7 +35,12 @@ function App() {
 
   return (
     <div>
-      <div className="price">{price}</div>
+      <div className="price">
+        {[
+          "price",
+          prevPrice < price ? "green" : prevPrice > price ? "red" : "",
+        ]}
+      </div>
       <div className="price-time">
         {priceTime && priceTime.toLocaleTimeString()}
       </div>
